7fd2f5e107a0d725911bb3880ac179a16657c19e
sk.you1226 <you1226@naver.com>
2022-02-19 16:17:12 +0900

    Initial commit

---
+++ app/src/main/java/com/knta/uip_20220219/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.knta.uip_20220219
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
